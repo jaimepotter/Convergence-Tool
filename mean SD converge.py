1432665ba70670bae9cb568d7c9263f90d8823df
Graham Potter <gpotterj@gmail.com>
2017-04-26 11:10:28 -0400

    Adjusted inputs to be incorporated into GUI
    Same inputs, but incorporated into interface for real time adjusting

--- mean SD converge.py
+++ mean SD converge.py
@@ -7,43 +7,43 @@ Created on Wed Mar  29 16:05:57 2017
 
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.widgets import Slider, Button
+from matplotlib.widgets import Slider, Button, RadioButtons
 
-def ask():
-    
-    # Cue users to type in the distribution they are drawing from
-    distribution = input("What distribution do you want to sample from? ")
-    
-    if distribution.lower() in dir(np.random):
-        if distribution.lower() == "normal":
-            param1 = input("What mean do you want to use for the normal distribution? ")
-            param2 = input("What standard deviation do you want to use for the normal distribution? ")
-            param3 = input("What sample size do you want to use for the normal distribution? ")
-        elif distribution.lower() == "logistic":
-            param1 = input("What mean do you want to use for the logistic distribution? ")
-            param2 = input("What scale do you want to use for the logistic distribution? (greater than 0): ")
-            param3 = input("What sample size do you want to use for the logistic distribution? ")
-        else:
-            print("For now, this just supports 'normal' and 'logistic' distributions. Please go back and select one of these!")
-        return((distribution.lower(),int(param1),int(param2),int(param3)))
-
-    else:
-        print("\nThis distribution is not available in numpy's random module. Please select another one from the below:\n\n", ", ".join(dir(np.random)[16:]),"\n\n")
-        ask()
+#def ask():
+#    
+#    # Cue users to type in the distribution they are drawing from
+#    distribution = input("What distribution do you want to sample from? ")
+#    
+#    if distribution.lower() in dir(np.random):
+#        if distribution.lower() == "normal":
+#            param1 = input("What mean do you want to use for the normal distribution? ")
+#            param2 = input("What standard deviation do you want to use for the normal distribution? ")
+#            param3 = input("What sample size do you want to use for the normal distribution? ")
+#        elif distribution.lower() == "logistic":
+#            param1 = input("What mean do you want to use for the logistic distribution? ")
+#            param2 = input("What scale do you want to use for the logistic distribution? (greater than 0): ")
+#            param3 = input("What sample size do you want to use for the logistic distribution? ")
+#        else:
+#            print("For now, this just supports 'normal' and 'logistic' distributions. Please go back and select one of these!")
+#        return((distribution.lower(),int(param1),int(param2),int(param3)))
+#
+#    else:
+#        print("\nThis distribution is not available in numpy's random module. Please select another one from the below:\n\n", ", ".join(dir(np.random)[16:]),"\n\n")
+#        ask()
 
 # Call ask() function and store the user-selected distribution in a variable
-dist_tup = ask()
-dist = dist_tup[0]
-param1 = dist_tup[1]
-param2 = dist_tup[2]
-param3 = dist_tup[3]
+#dist_tup = ask()
+#dist = dist_tup[0]
+#param1 = dist_tup[1]
+#param2 = dist_tup[2]
+#param3 = dist_tup[3]
 
 def update(val):
-    sigma = setSD.val
+    sigma = setsigma.val
+    mu = setmu.val
+    N = int(setN.val)
     plots.clear()
     del ax.lines[:];
-    # sample size for slider 
-    N = 100
     # num of trials to average error of SD and mean over
     numTimes = 300
     
@@ -53,7 +53,7 @@ def update(val):
     mtot = []
     sdtot = []
     while i < numTimes:
-        s = eval("np.random."+dist+"("+str(param1)+", "+str(param2)+", "+str(param3)+")")
+        s = eval("np.random." + rb.value_selected + "("+str(mu)+", "+str(sigma)+", "+str(N)+")")
         i2 = 1
         m = []
         sd = []
@@ -99,11 +99,11 @@ def update(val):
     print('here')
     return mtot
 
-fig5 = plt.figure(figsize=(14,7),edgecolor = 'black',frameon=False,tight_layout=True)
+fig5 = plt.figure(figsize=(14,7),edgecolor = 'black',frameon=False,tight_layout=False)
 ax4 = plt.subplot(111)
-fig2 = plt.figure(figsize=(14,7),edgecolor = 'black',frameon=False,tight_layout=True)
-mu, sigma = 1, .68 # mean and standard deviation
-s = eval("np.random."+dist+"("+str(param1)+", "+str(param2)+", "+str(param3)+")")
+fig2 = plt.figure(figsize=(14,7),edgecolor = 'black',frameon=False,tight_layout=False)
+mu, sigma, N = 1, .68, 100 # mean and standard deviation
+s = eval("np.random." + 'normal' + "("+str(mu)+", "+str(sigma)+", "+str(N)+")")
 mean = []
 SD = []
 plots = []
@@ -116,16 +116,25 @@ while i < len(s):
 count, bins, ignored = plt.hist(s, 30, normed=True)
 p1 = plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ),linewidth=2, color='r')
 fig = plt.figure(figsize=(14,7),edgecolor = 'black',frameon=False,tight_layout=False)
-ax = plt.axes([0.05,.2,.9,.7]) 
+ax = plt.axes([0.05,.2,.8,.7]) 
 plt.title('comparison for '+str(sigma)+' SD')
 plt.ylabel('percent error')
 plt.xlabel('sample size N')
 mean, = plt.plot(mean,label='mean error',color='red')
 SD, = plt.plot(SD,label='SD error',color='blue')
 legend = ax.legend(loc='right center', shadow=True)
-axSlide = plt.axes([0.1,.05,.8,.05]) 
-setSD = Slider(axSlide, 'Set SD', 0.1, 10, valinit=sigma)
 
 
-setSD.on_changed(update)
+axSlide = plt.axes([0.1,.1,.8,.05]) 
+setsigma = Slider(axSlide, 'Set SD', 0.1, 10, valinit=sigma)
+axSlide2 = plt.axes([0.1,.05,.8,.05]) 
+setmu = Slider(axSlide2, 'Set mu', 0.1, 100, valinit=mu)
+axSlide3 = plt.axes([0.1,.01,.8,.05]) 
+setN = Slider(axSlide3, 'Set N', 1, 1000, valinit=N)
+rb = plt.axes([.905, 0.7, 0.09, 0.11],axisbg=(.5,.5,.5,.2))
+rb = RadioButtons(rb, ('normal','logistic'))
+rb.on_clicked(update)
+
+
+setsigma.on_changed(update)
 plt.show()
